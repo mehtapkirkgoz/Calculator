fad2a7c1ab05b04961278b9905a098374f82adb6
Mehtap Kırkgöz <mhtpkrkgz22@gmail.com>
2026-04-30 22:53:48 +0300

    change math functions

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ let firstValue = "";
 let secondValue = "";
 let currentOperator = null;
 let shouldReset = false;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "Error";
+  return a / b;
+}
